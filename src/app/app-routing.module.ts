ee3115fb1d496732153146d54499d4e111fe467b
AyseCKR69 <ayseckr699@gmail.com>
2022-11-23 21:51:55 +0300

    21:51

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,24 +9,18 @@ import { UserAddComponent } from './pages/users/user-add/user-add.component';
 import { UserListComponent } from './pages/users/user-list/user-list.component';
 
 const routes: Routes = [
-  {path:"home",
-   component:HomeComponent},
-   {path:"product-add",
-   component:ProductAddComponent},
-   {path:"app-category-add",
-  component:CategoryAddComponent},
-  {path:"app-product-list",
-  component:ProductListComponent},
-  {path:"app-user-list",
-  component:UserListComponent},
-  {path:"app-user-add",
-  component:UserAddComponent},
-  {path:"app-category-list",
-  component:CategoryListComponent}
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'product-add', component: ProductAddComponent },
+  { path: 'app-category-add', component: CategoryAddComponent },
+  { path: 'app-product-list', component: ProductListComponent },
+  { path: 'app-user-list', component: UserListComponent },
+  { path: 'app-user-add', component: UserAddComponent },
+  { path: 'app-category-list', component: CategoryListComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
